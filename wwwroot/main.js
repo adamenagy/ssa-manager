9ddb0e8affc488a3cf80f6473755146198a0cc03
Adam Nagy <adam_e_nagy@hotmail.com>
2025-01-22 11:24:54 +0000

    create token works

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,8 +1,12 @@
+const jwt = window.jwt;
+
 let accessToken = null;
 
+// Authentication
+
 function getAccessToken() {
-    const clientId = document.getElementById("clientId").value;
-    const clientSecret = document.getElementById("clientSecret").value;
+    const clientId = document.getElementById("client-id").value;
+    const clientSecret = document.getElementById("client-secret").value;
     const ssaScopes =
         "application:service_account:read application:service_account:write application:service_account_key:read application:service_account_key:write";
 
@@ -24,11 +28,15 @@ function getAccessToken() {
     });
 }
 const login = document.getElementById("login");
-login.onclick = async function () {
+login.onclick = async () => {
     accessToken = await getAccessToken();
 
     listAccounts();
 };
+
+// Accounts
+
+const accountsList = document.getElementById("accounts-list");
 
 async function listAccounts() {
     const res = await fetch(
@@ -43,7 +51,6 @@ async function listAccounts() {
 
     const data = await res.json();
 
-    const accountsList = document.getElementById("accounts-list");
     accountsList.innerHTML = "";
     data.serviceAccounts.forEach((account) => {
         let option = document.createElement("option");
@@ -52,18 +59,61 @@ async function listAccounts() {
         option.data = account;
         accountsList.appendChild(option);
     });
-    accountsList.onchange = function () {
-        const selectedAccount =
+    accountsList.onchange = () => {
+        const accountData =
             accountsList.options[accountsList.selectedIndex].data;
         document.getElementById("account-details").innerHTML = JSON.stringify(
-            selectedAccount,
+            accountData,
             null,
             2
         );
 
-        listKeys(selectedAccount.serviceAccountId);
+        listKeys(accountData.serviceAccountId);
     };
 }
+
+function addAccount(account) {
+    let option = document.createElement("option");
+    option.value = account.serviceAccountId;
+    option.text = account.email;
+    option.data = account;
+    accountsList.appendChild(option);
+}
+
+function removeAccount() {
+    accountsList.remove(accountsList.selectedIndex);
+}
+
+const createAccount = document.getElementById("create-account");
+createAccount.onclick = async () => {
+    const accountName = document.getElementById("account-name").value;
+
+    try {
+        const res = await fetch(
+            "https://developer.api.autodesk.com/authentication/v2/service-accounts",
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify({
+                    name: accountName,
+                }),
+            }
+        );
+
+        const data = await res.json();
+        console.log(data);
+
+        addAccount(data);
+    } catch (error) {
+        alert(error);
+    }
+};
+
+// Keys
+
+const keysList = document.getElementById("keys-list");
 
 async function listKeys(accountId) {
     const res = await fetch(
@@ -78,23 +128,98 @@ async function listKeys(accountId) {
 
     const data = await res.json();
 
-    const keysList = document.getElementById("keys-list");
     keysList.innerHTML = "";
     data.keys.forEach((key) => {
-        let option = document.createElement("option");
-        option.value = key.kid;
-        option.text = key.kid;
-        option.data = key;
-        keysList.appendChild(option);
-    });
-    keysList.onchange = function () {
-        const selectedKey = keysList.options[keysList.selectedIndex].data;
+		addKey(key);
+	});
+    keysList.onchange = () => {
+        const keyData = keysList.options[keysList.selectedIndex].data;
         document.getElementById("key-details").innerHTML = JSON.stringify(
-            selectedKey,
+            keyData,
             null,
             2
         );
 
-        listKeys(selectedAccount.serviceAccountId);
+        showPrivateKey(keyData.kid);
     };
 }
+
+function addKey(key) {
+    let option = document.createElement("option");
+    option.value = key.kid;
+    option.text = key.kid;
+    option.data = key;
+    keysList.appendChild(option);
+}
+
+async function showPrivateKey(kid) {
+    const accountData = accountsList.options[accountsList.selectedIndex].data;
+
+    const privateKey = accountData[kid]?.privateKey || "";
+
+    document.getElementById("private-key").value = privateKey;
+}
+
+const loadPem = document.getElementById("load-pem");
+loadPem.onclick = () => {
+    const fileInput = document.getElementById("file-input");
+    fileInput.click();
+
+    fileInput.onchange = function () {
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => {
+            document.getElementById("private-key").value = reader.result;
+            fileInput.value = "";
+        };
+    };
+};
+
+// Tokens
+
+const createToken = document.getElementById("create-token");
+createToken.onclick = async () => {
+    const accountData = accountsList.options[accountsList.selectedIndex].data;
+
+    const keyData = keysList.options[keysList.selectedIndex].data;
+
+    const privateKey = document.getElementById("private-key").value;
+
+    const clientId = document.getElementById("client-id").value;
+    const clientSecret = document.getElementById("client-secret").value;
+
+    const scopes = document.getElementById("scopes").value;
+
+    const payload = {
+        kid: keyData.kid,
+        privateKey,
+        clientId,
+        clientSecret,
+        accountId: accountData.serviceAccountId,
+        scope: scopes,
+    };
+
+    try {
+        const token = await fetch(`/token`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (!token.ok)
+            throw new Error(`HTTP error! status: ${await token.text()}`);
+
+        const tokenData = await token.json();
+
+        document.getElementById("access-token").innerHTML = JSON.stringify(
+            tokenData,
+            null,
+            2
+        );
+    } catch (error) {
+        alert(error);
+    }
+};
